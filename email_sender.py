e57aa21610ecf9f83a0e802cedb433c466cb934b
Prakhar Bajpai <63141512+Prakharbajpai07@users.noreply.github.com>
2020-07-03 22:01:15 +0530

    Update email_sender.py

--- email_sender.py
+++ email_sender.py
@@ -3,13 +3,13 @@ from email.message import EmailMessage
 
 email = EmailMessage()
 email['from'] = 'Prakhar Bajpai'
-email['to'] = 'prakharalways7@gmail.com'
+email['to'] = 'Email'
 email['subject'] = 'you won 300000 prakhar'
 email.set_content('I am invatable')
 
 with smtplib.SMTP(host = 'smtp.gmail.com', port=587) as smtp:
         smtp.ehlo()
         smtp.starttls()
-        smtp.login("prakharalways1999@gmail.com", "Kanku@1423")
+        smtp.login("email", "password")
         smtp.send_message(email)
         print("all good boss")
